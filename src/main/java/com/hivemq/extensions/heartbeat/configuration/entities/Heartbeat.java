a5e93897132486fa1fb6cd7f3dca0e63b536130a
David Sondermann <david.sondermann@hivemq.com>
2026-02-13 13:05:43 +0100

    Update JAXB dependencies to use Jakarta EE version

--- src/main/java/com/hivemq/extensions/heartbeat/configuration/entities/Heartbeat.java
+++ src/main/java/com/hivemq/extensions/heartbeat/configuration/entities/Heartbeat.java
@@ -15,13 +15,12 @@
  */
 package com.hivemq.extensions.heartbeat.configuration.entities;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
 import org.jetbrains.annotations.NotNull;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
 
 /**
  * JAXB entity representing the heartbeat extension configuration.
